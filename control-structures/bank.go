cf5e495f1e77aa42c5960892201293fb186ca27a
hassanjawwad12 <hassanjawwad01@gmail.com>
2025-02-28 16:32:21 +0500

    ✅ Added Loop

--- control-structures/bank.go
+++ control-structures/bank.go
@@ -6,48 +6,49 @@ func main() {
 	var balance float64 = 1000.50
 
 	fmt.Println("Welcome to the Bank of Golang")
-	fmt.Println("What do u want to do?")
-	fmt.Println("1. Check Balance")
-	fmt.Println("2. Deposit")
-	fmt.Println("3. Withdraw")
-	fmt.Println("4. Exit")
 
-	var choice int
-	fmt.Print("Enter your choice: ")
-	fmt.Scanln(&choice)
+	for {
+		fmt.Println("What do u want to do?")
+		fmt.Println("1. Check Balance")
+		fmt.Println("2. Deposit")
+		fmt.Println("3. Withdraw")
+		fmt.Println("4. Exit")
 
-	wantBalace := choice == 1
-	wantDeposit := choice == 2
-	wantWithdraw := choice == 3
-	wantExit := choice == 4
+		var choice int
+		fmt.Print("Enter your choice: ")
+		fmt.Scanln(&choice)
 
-	if wantBalace {
-		fmt.Println("You have balance in your account", balance)
-	} else if wantDeposit {
-		var deposit float64
-		fmt.Print("Enter the amount to deposit: ")
-		fmt.Scanln(&deposit)
-		if deposit < 0 {
-			fmt.Println("Invalid amount")
-			return
-		}
-		balance += deposit
-		fmt.Println("You have deposited", deposit, "and your new balance is", balance)
-	} else if wantWithdraw {
-		var withdraw float64
-		fmt.Print("Enter the amount to withdraw: ")
-		fmt.Scanln(&withdraw)
-		if withdraw > balance {
-			fmt.Println("Insufficient balance")
-			return
+		wantBalace := choice == 1
+		wantDeposit := choice == 2
+		wantWithdraw := choice == 3
+
+		if wantBalace {
+			fmt.Println("You have balance in your account", balance)
+		} else if wantDeposit {
+			var deposit float64
+			fmt.Print("Enter the amount to deposit: ")
+			fmt.Scanln(&deposit)
+			if deposit < 0 {
+				fmt.Println("Invalid amount")
+				continue
+			}
+			balance += deposit
+			fmt.Println("You have deposited", deposit, "and your new balance is", balance)
+		} else if wantWithdraw {
+			var withdraw float64
+			fmt.Print("Enter the amount to withdraw: ")
+			fmt.Scanln(&withdraw)
+			if withdraw > balance {
+				fmt.Println("Insufficient balance")
+				continue
+			} else {
+				balance -= withdraw
+				fmt.Println("You have withdrawn", withdraw, "and your new balance is", balance)
+			}
 		} else {
-			balance -= withdraw
-			fmt.Println("You have withdrawn", withdraw, "and your new balance is", balance)
+			fmt.Println("Thank you for using the Bank of Golang")
+			break
 		}
-	} else if wantExit {
-		fmt.Println("Thank you for using the Bank of Golang")
-	} else {
-		fmt.Println("Invalid choice")
 	}
 
 }
