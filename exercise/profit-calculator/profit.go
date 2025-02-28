c7b939c1e5bf14d54c391ef50bca5187e08167b3
hassanjawwad12 <hassanjawwad01@gmail.com>
2025-02-28 18:07:55 +0500

    ✅ Added Exercise Solution

--- exercise/profit-calculator/profit.go
+++ exercise/profit-calculator/profit.go
@@ -1,23 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
+
+func outputText(name string) (float64, error) {
+	var value float64
+	fmt.Printf("Enter the %s : ", name)
+	fmt.Scan(&value)
+	if value <= 0 {
+		return 0, errors.New("invalid input")
+	}
+	return value, nil
+}
+
+func performCalculation(revenue, expenses, taxRate float64) (profit, tax, netProfit, ratio float64) {
+	profit = revenue - expenses
+	tax = profit * taxRate / 100
+	netProfit = profit - tax
+	ratio = netProfit / profit
+	return
+}
+
+func storeResults(profit, tax, netProfit, ratio float64) (float64, float64, float64, float64) {
+	data := fmt.Sprintf("Profit is %.2f\nTax is %.2f\nNet profit is %.2f\nNet profit ratio is %.2f\n", profit, tax, netProfit, ratio)
+	os.WriteFile("profit.txt", []byte(data), 0644)
+	return profit, tax, netProfit, ratio
+}
 
 func main() {
 
 	fmt.Println("Profit Calculator")
-	var revenue, expenses, taxRate float64
 
-	fmt.Print("Enter the revenue: ")
-	fmt.Scan(&revenue)
-	fmt.Print("Enter the expenses: ")
-	fmt.Scan(&expenses)
-	fmt.Print("Enter the tax rate: ")
-	fmt.Scan(&taxRate)
+	revenue, err := outputText("Revenue")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-	profit := revenue - expenses
-	tax := profit * taxRate / 100
-	netProfit := profit - tax
-	ratio := netProfit / profit
+	expenses, err := outputText("Expenses")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	taxRate, err := outputText("Tax Rate")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	profit, tax, netProfit, ratio := performCalculation(revenue, expenses, taxRate)
 
 	fmt.Printf("Profit is %.2f\nTax is %.2f\nNet profit is %.2f\nNet profit ratio is %.2f\n", profit, tax, netProfit, ratio)
+	storeResults(profit, tax, netProfit, ratio)
 }
