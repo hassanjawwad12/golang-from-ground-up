79536018631fa3cc74b4c1d3292be4b66490b042
hassanjawwad12 <hassanjawwad01@gmail.com>
2025-03-03 11:48:19 +0500

    ✅ Added Separate Package for User Struct

--- structs/struct.go
+++ structs/struct.go
@@ -2,39 +2,9 @@ package main
 
 import (
 	"fmt"
-	"time"
+
+	"github.com/hassanjawwad12/golang-from-ground-up/user"
 )
-
-// define your own user type
-type user struct {
-	firstName string
-	lastName  string
-	birthDate string
-	createdAt time.Time
-}
-
-// struct constructor (just a convention)
-func newUser(firstName, lastName, birthDate string) *user {
-	return &user{
-		firstName: firstName,
-		lastName:  lastName,
-		birthDate: birthDate,
-		createdAt: time.Now(),
-	}
-}
-
-// struct method to display user data
-func (u *user) displayUser() {
-	fmt.Println("First Name:", u.firstName)
-	fmt.Println("Last Name:", u.lastName)
-	fmt.Println("Birth Date:", u.birthDate)
-	fmt.Println("Created At:", u.createdAt)
-}
-
-// struct method to change user name
-func (u *user) changeName() {
-	u.firstName = getUserData("Please enter your first name again: ")
-}
 
 func getUserData(promptText string) string {
 	fmt.Print(promptText)
@@ -48,9 +18,12 @@ func main() {
 	lastName := getUserData("Please enter your last name: ")
 	birthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
 
-	// create a new user using constructor
-	appuser := newUser(firstName, lastName, birthdate)
-	appuser.displayUser()
-	appuser.changeName()
-	appuser.displayUser()
+	appuser, err := user.NewUser(firstName, lastName, birthdate)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	appuser.DisplayUser()
+	appuser.ChangeName()
+	appuser.DisplayUser()
 }
---
+++ user/user.go
@@ -0,0 +1,39 @@
+package user
+
+import (
+	"errors"
+	"fmt"
+	"time"
+)
+
+// define your own user type
+type User struct {
+	FirstName string
+	LastName  string
+	BirthDate string
+	CreatedAt time.Time
+}
+
+// struct constructor (just a convention)
+func NewUser(firstName, lastName, birthDate string) (*User, error) {
+	if firstName == "" || lastName == "" || birthDate == "" {
+		return nil, errors.New("first name, last name and birthdate are required")
+	}
+	return &User{
+		FirstName: firstName,
+		LastName:  lastName,
+		BirthDate: birthDate,
+		CreatedAt: time.Now(),
+	}, nil
+}
+
+func (u *User) DisplayUser() {
+	fmt.Println("First Name:", u.FirstName)
+	fmt.Println("Last Name:", u.LastName)
+	fmt.Println("Birth Date:", u.BirthDate)
+	fmt.Println("Created At:", u.CreatedAt)
+}
+
+func (u *User) ChangeName() {
+	u.FirstName = "Mark"
+}
